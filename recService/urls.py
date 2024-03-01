56a478b9efecb10595f3b9897fe8e6f47935923a
Manuel Bojorge <bojorgearayamanuel@gmail.com>
2024-02-29 23:39:26 -0600

    just leave the test endpoint

--- recService/urls.py
+++ recService/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('test/', views.test, name='test'),
-    path('default/<str:type>/<str:value>', views.default, name='default'),
-    path('artificial/<str:type>/<str:value>', views.artificial, name='artificial'),
-    path('externalSystem/<str:type>/<str:value>', views.externalSystem, name='externalSystem'),
-    path('data/', views.data, name='data'),
+    #path('default/<str:type>/<str:value>', views.default, name='default'),
+    #path('artificial/<str:type>/<str:value>', views.artificial, name='artificial'),
+    #path('externalSystem/<str:type>/<str:value>', views.externalSystem, name='externalSystem'),
+    #path('data/', views.data, name='data'),
 ]
