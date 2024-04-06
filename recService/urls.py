d2b053db0d4fb015fdfa09787977e1004799d4af
Bojorge <bojorgearayamanuel@gmail.com>
2024-04-06 16:31:59 -0600

    Updating repo

--- recService/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    #path('default/<str:type>/<str:value>', views.default, name='default'),
-    #path('artificial/<str:_type>/<str:value>', views.artificial, name='artificial'),
-    #path('externalSystem/<str:type>/<str:value>', views.externalSystem, name='externalSystem'),
-    path('data/', views.data, name='data'),
-]
